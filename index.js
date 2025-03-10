48e6feecc7d1b9f59e402714238441684c1e2f5f
Joel Mwaga <mwagahjoel14@gmail.com>
2025-03-10 15:02:59 +0300

    changed 1 file

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+var upperCaseCustomerName = () => {
+  customerName = customerName.toUpperCase();
+}
+var setBestCustomer = () => {
+  bestCustomer = 'not bob';
+}
+var overwriteBestCustomer = () => {
+  bestCustomer = 'maybe bob';
+}
+var changeLeastFavoriteCustomer = () => {
+  leastFavoriteCustomer = 'new customer'; // This will throw an error
+}
+const leastFavoriteCustomer = 'bob';
+var attemptTwoFavoriteCustomers = () => {
+    let favoriteCustomer = 'bob';
+    // let favoriteCustomer = 'not bob'; // Uncommenting this will throw an error
+}
+
